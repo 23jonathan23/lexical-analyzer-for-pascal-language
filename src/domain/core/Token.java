1241171ceeb9a3d54a67905f1ccc8ad8a4ef3ec0
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-09-19 20:29:16 -0300

    Token and TokenType core added

---
+++ src/domain/core/Token.java
@@ -0,0 +1,97 @@
+package src.domain.core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Token {
+    private final String REGEX_IDENTIFIER = "[a-zA-Z_][a-zA-Z_0-9]*";
+
+    private final String REGEX_NUMBER = "\\d+";
+
+    private final String REGEX_NSTRING = "\"([^\"]*)\"";
+
+    private List<String> symbols;
+
+    private List<String> operators;
+
+    private List<String> reservedWords;
+
+    private List<String> tokens;
+
+    private List<String> invalidTokens;
+
+    public Token() {
+        this.reservedWords = Arrays.asList("var", "string", "char", "boolean", "integer", "real", "if", "then", 
+            "else", "begin", "end", "read", "writeln", "program", "uses", "crt");
+        
+        this.operators = Arrays.asList("+", "-", "/", "*", "%", "^");
+
+        this.symbols = Arrays.asList("<", ">", ":", "|", "&");
+
+        this.tokens = new ArrayList<>();
+
+        this.invalidTokens = new ArrayList<>();
+    }
+
+    public void mapTokensId(List<String> tokens) {
+        for(var token : tokens)
+            if(Pattern.matches(REGEX_IDENTIFIER, token) && !this.reservedWords.contains(token)
+                && !Pattern.matches(REGEX_NSTRING, token))
+                this.tokens.add(TokenType.ID.toString() + ", " + token);
+    }
+
+    public void mapTokensNumber(List<String> tokens) {
+        for(var token : tokens)
+            if(Pattern.matches(REGEX_NUMBER, token)) 
+                this.tokens.add(TokenType.NUMBER.toString() + ", " +token);
+    }
+
+    public void mapTokensOperator(List<String> tokens) {
+        for(var token : tokens)
+            if(this.operators.contains(token)) 
+                this.tokens.add(TokenType.OPERATOR.toString() + ", " + token);
+    }
+
+    public void mapTokensReservedWords(List<String> tokens) {
+        for(var token : tokens)
+            if(this.reservedWords.contains(token)) 
+                this.tokens.add(TokenType.KEYWORD.toString() + ", " + token);
+    }
+
+    public void mapTokensSymbol(List<String> tokens) {
+        for(var token : tokens)
+            if(this.symbols.contains(token)) 
+                this.tokens.add(TokenType.SYMBOL.toString() + ", " + token);
+    }
+
+    public void mapTokensParameter(List<String> tokens) {
+        for(var token : tokens)
+            if(token.equals("("))
+                this.tokens.add(TokenType.LPAR.toString() + ", " + token);
+            else if(token.equals(")"))
+                this.tokens.add(TokenType.RPAR.toString() + ", " + token);
+    }
+
+    public void mapTokensNString(List<String> tokens) {
+        for(var token : tokens)
+            if(Pattern.matches(REGEX_NSTRING, token)) 
+                this.tokens.add(TokenType.NSTRING.toString() + ", " + token);
+    }
+
+    public void mapTokensAssignment(List<String> tokens) {
+        for(var token : tokens)
+            if(token.equals("="))
+                this.tokens.add(TokenType.ASSIGNMENT.toString() + ", " + token);
+    }
+
+    public void mapInvalidTokens(List<String> tokens) {
+        for(var token : tokens)
+            this.invalidTokens.add(TokenType.INVALID.toString() + ", " + token);
+    }
+
+    public List<String> getTokensResult() {
+        return this.tokens;
+    }
+}
---
+++ src/domain/core/TokenType.java
@@ -0,0 +1,14 @@
+package src.domain.core;
+
+public enum TokenType {
+    ID,
+    NUMBER,
+    ASSIGNMENT,
+    OPERATOR,
+    LPAR,
+    RPAR,
+    NSTRING,
+    KEYWORD,
+    SYMBOL,
+    INVALID
+}
